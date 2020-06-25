bb25a344c983899fd5b4294629981869ccc69f08
gabs-dev <gabrielfelipe1458@gmail.com>
2020-06-25 19:48:32 -0300

    Obtendo um usuárioio por id

--- src/main/java/com/gabriel/workshopmongo/services/UserService.java
+++ src/main/java/com/gabriel/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.gabriel.workshopmongo.services;
 
 import com.gabriel.workshopmongo.domain.User;
 import com.gabriel.workshopmongo.repository.UserRepository;
+import com.gabriel.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 
 }
