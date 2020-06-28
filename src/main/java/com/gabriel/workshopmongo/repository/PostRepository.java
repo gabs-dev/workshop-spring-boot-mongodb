b4cb9ab2066e1c1eaf15be7685f7b2fd4bbcf7f8
gabs-dev <gabrielfelipe1458@gmail.com>
2020-06-28 15:02:08 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/gabriel/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.gabriel.workshopmongo.repository;
+
+import com.gabriel.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
