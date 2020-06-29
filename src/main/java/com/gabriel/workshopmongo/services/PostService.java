dbce63fa77f21b9c06dafa136d58ce6bc1bdc368
gabs-dev <gabrielfelipe1458@gmail.com>
2020-06-28 23:12:07 -0300

    Consulta simples s com query methods

--- src/main/java/com/gabriel/workshopmongo/services/PostService.java
+++ src/main/java/com/gabriel/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.gabriel.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,10 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repository.findByTitleContainingIgnoreCase(text);
     }
 
 }
