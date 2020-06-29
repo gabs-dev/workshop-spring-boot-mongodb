6e94e1b9de5efdf6b5e7196faf2b9649a3ced1a8
gabs-dev <gabrielfelipe1458@gmail.com>
2020-06-28 23:27:46 -0300

    Consulta simples com @Query

--- src/main/java/com/gabriel/workshopmongo/services/PostService.java
+++ src/main/java/com/gabriel/workshopmongo/services/PostService.java
@@ -21,7 +21,7 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text) {
-        return repository.findByTitleContainingIgnoreCase(text);
+        return repository.searchTitle(text);
     }
 
 }
